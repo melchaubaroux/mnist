8676bbf0e9fdfc9e03a8ae34055a62fcb93dbb63
melchaubaroux <mel.chaubaroux@gmail.com>
2024-03-04 02:09:49 +0100

    fix

--- API_pred_mnist.py
+++ API_pred_mnist.py
@@ -3,38 +3,21 @@ from typing import Annotated
 import requests
 import uvicorn
 from bs4 import BeautifulSoup
-# from transformers import AutoTokenizer, AutoModelForCausalLM
+
 import pickle 
 
-# # load model gemma
-# huggingface_token = "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"
-# tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it", use_auth_token=huggingface_token)
-# model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it", use_auth_token=huggingface_token)
 
 # load model mnist
-digit=open("mnist_digit",'rb')
+digit=open("trained_model/mnist_digit",'rb')
 
 
 digit = pickle.load(digit)
 
 # load model emnist
-letter=open("mnist_letter",'rb')
+letter=open("trained_model/mnist_letter",'rb')
 letter = pickle.load(letter)
 
 
-def send_text (text): return 0
-
-
-    # input_text = text
-    # input_ids = tokenizer(input_text, return_tensors="pt")
-
-    # outputs = model.generate(**input_ids)
-    # # print(tokenizer.decode(outputs[0]))
-
-    # return tokenizer.decode(outputs[0])
-
-
-    
 
 def send_image (image):
 
@@ -68,11 +51,6 @@ async def create_upload_file(image: UploadFile):
     return send_image(image)
 
 
-@app.post("/chatbot")
-def chatbot(text):
-    return send_text (text)  
-
-    
 uvicorn.run(app)
 
 
--- fonction_ressources/image_formatting.py
+++ fonction_ressources/image_formatting.py
@@ -7,17 +7,16 @@ def image_formatting (path,dim):
     
     #cropping 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
-    max_contour = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(max_contour)
-    cropped = img[y:y+h, x:x+w]
+    # _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
+    # contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
+    # max_contour = max(contours, key=cv2.contourArea)
+    # x, y, w, h = cv2.boundingRect(max_contour)
+    # cropped = img[y:y+h, x:x+w]
     
     # resize
-    resize=cv2.resize(cropped, (dim[0],dim[1]), interpolation = cv2.INTER_AREA)
+    resize=cv2.resize(gray, (dim[0],dim[1]), interpolation = cv2.INTER_AREA)
      
-    # Appliquer un flou gaussien pour réduire le bruit
-    blurred = cv2.GaussianBlur(resize, (5, 5), 0)
+
 
     # sauvegarde 
-    cv2.imwrite(path, blurred)  
+    cv2.imwrite(path, resize)  
